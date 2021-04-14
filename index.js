f273d476deef9500e2bf99088e23ee2dfd6f25e7
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-14 10:35:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-//function greet("content" )
-//{ 
+function greet(time)
+{ 
 
 
 
